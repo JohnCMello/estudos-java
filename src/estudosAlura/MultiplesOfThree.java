c255dc41835e419d8e404a2a2d4f4a2d8a3b3138
JohnCMello <johncharlesmello@gmail.com>
2024-04-01 19:28:26 -0300

    exercicio 5

---
+++ src/estudosAlura/MultiplesOfThree.java
@@ -0,0 +1,16 @@
+package estudosAlura;
+
+public class MultiplesOfThree {
+
+	public static void main(String[] args) {
+		int finish = 100;
+		
+		for(int index = 1; index <= finish; index++) {
+			if(index % 3 == 0) {
+				System.out.println(index);
+			}
+		}
+
+	}
+
+}
