28c8b1d5d4c157bb8bd0ab1137b38a57b63a03f7
JohnCMello <johncharlesmello@gmail.com>
2024-04-01 19:22:33 -0300

    exercicio 4

---
+++ src/estudosAlura/AddUpNumbers.java
@@ -0,0 +1,18 @@
+package estudosAlura;
+
+public class AddUpNumbers {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int index = 0;
+		int finish = 1000;
+		
+		while (index <= finish) {
+			sum += index;
+			index++;
+		}
+		
+		System.out.println(sum);
+	}
+
+}
