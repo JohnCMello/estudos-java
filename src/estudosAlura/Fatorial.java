90867800f9336cd52c0185c966540fbcf0307f55
JohnCMello <johncharlesmello@gmail.com>
2024-04-01 22:22:34 -0300

    exercicio 7

--- src/estudosAlura/Fatorial.java
+++ src/estudosAlura/Fatorial.java
@@ -3,7 +3,7 @@ package estudosAlura;
 public class Fatorial {
 
 	public static void main(String[] args) {
-		int finish = 10;
+		int finish = 30;
 		for (int i = 0; i <= finish; i++) {
             long fatorial = 1;
             for (int j = 1; j <= i; j++) {
